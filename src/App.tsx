39e38b7f87b1e2689609bd6bd91322a9c1163e73
Dan Walsh <dan.walsh@outlook.com.au>
2020-09-21 12:40:13 +1000

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+
+import quotes from './quotes.json';
 
 function App() {
+  var quote = quotes[Math.floor(Math.random() * quotes.length)];
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app loaded">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +22,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div id="quote">{quote.quote}</div>
+      <div id="source">{quote.source}</div>
     </div>
   );
 }
