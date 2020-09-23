2352307fa57b93a8ffcf43993afc0b7d6bc58c02
Dan Walsh <danwalsh@Dans-MacBook-Pro.local>
2020-09-23 12:42:06 +1000

    Refactored to clean up logic.

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,38 @@ import './App.scss';
 // Import entries
 import entries from './entries.json';
 
+type Entry = {
+  quote: string
+  source: string
+  class: string
+}
+
 function App() {
-  const quote = entries[Math.floor(Math.random() * entries.length)];
-  const words = quote.quote.split(' ').length;
-  const quoteClass = (words <= 10) ? 'quote-sm' : 'quote-lg';
+  const getEntryClass = (entryText: string) => {
+    const totalWords = entryText.split(' ').length;
+    return (totalWords <= 10) ? 'quote-sm' : 'quote-lg';
+  };
 
-  let randomEntry = entries[Math.floor(Math.random() * entries.length)];
+  const pickRandomEntry = (): Entry => {
+    let randomEntry = entries[Math.floor(Math.random() * entries.length)];
+    return {
+      quote: randomEntry.quote,
+      source: randomEntry.source,
+      class: getEntryClass(randomEntry.quote)
+    };
+  };
+  
+  const [entry, setEntry] = useState(pickRandomEntry());
 
-  const [entry, setEntry] = useState({quote: randomEntry.quote, source: randomEntry.source});
+  const setRandomEntry = () => {
+    setEntry(pickRandomEntry());
+  };
   
   useEffect(() => {
     window.addEventListener('load', e => {
       document.getElementById('quotes')?.classList.add('loaded');
     });
   }, []);
-
-  const pickRandomEntry = () => {
-    let randomEntry = entries[Math.floor(Math.random() * entries.length)]
-    setEntry({quote: randomEntry.quote, source: randomEntry.source});
-  };
 
   return (
     <div id="app">
@@ -35,11 +48,11 @@ function App() {
         <PuffLoader color="#fff" size="70px" />
       </div>
       <div id="quotes">
-        <div id="quote" className={quoteClass}>{entry.quote}</div>
+        <div id="quote" className={entry.class}>{entry.quote}</div>
         <div id="source">{entry.source}</div>
       </div>
       {/* eslint-disable-next-line */}
-        <a id="close" href="#" onClick={pickRandomEntry}>
+        <a id="close" href="#" onClick={setRandomEntry}>
           <RefreshButton />
         </a>
     </div>
